5625318cc1c60077650d024ce6c79c6c3b69782c
Aphomer <aaphomer@gmail.com>
2023-07-25 13:14:45 +0100

    adding product controller

--- controller/products.js
+++ controller/products.js
@@ -6,45 +6,32 @@ module.exports.getNewCloth = (req, res) => {
 }
 
 module.exports.getAllClothes = async (req, res) => {
-    // let pageNumber = 2; // PAGINATION
-    // let pageSize = 10;
+    // let pageNumber = 1; // PAGINATION
+    // let pageSize = 2;
+    
     try {
-        const clothes = await Product.find({})
-
+        const clothes = await Product.find({ })
+        // const clothes = await Product.find({ id: req.user._id })
+        // const clothes = await Product.find({ }).populate('user')
+        
         // clothes //for PAGINATION
-            // .sort('deliveryDate')
+            .sort('deliveryDate')
             // .select('ownerName')
             // .skip((pageNumber - 1) * pageSize)
             // .limit(pageSize);
 
             
-
-        for(let cloth of clothes) {
-            console.log(cloth.ownerName.toUpperCase());
-            
-            // cloth.clothImages.forEach(image => {
-            //     console.log(image.url);
-            // });
-
-            
-// cloth.clothImages.forEach(image => {
-    
-//     <div class="allCloth-name">
-//         <a class="allCloth-name" href="/clothing/<%=cloth._id%>"><%=cloth.ownerName.toUpperCase()%></a>
-//     </div>
-//     <img crossorigin="anonymous" class="show" alt="...." src="<%=image.url%>">
-// });
-
+        if(!clothes) {
+            req.flash('error', 'No Clothes available');
+            res.send("No Clothes available");
         }
-        // console.log(clothes); 
-        res.render('cloth/allclothes', { clothes });
         
+        res.render('cloth/allclothes', { clothes });
     } catch (error) {
         console.log(error);
         res.send(`Error:${error}`)
     }
 }
-
 
 module.exports.howToMeasure = (req, res) => {
     res.render('cloth/howToMeasure');
@@ -52,22 +39,15 @@ module.exports.howToMeasure = (req, res) => {
 
 module.exports.createNewCloth = async (req, res) => {
     try {
-        const { ownerName, contact, price, deposit, deliveryDate, measurements, clothImages, leg, neck, waist, shoulder, arm,  chest, bicep, wrist, back, stomach, hip, thigh } = req.body
-        
-        // const cloth = new Product({ ...req.body })
-        
+        const { ownerName, contact, price, deposit, deliveryDate, measurements, clothImages, leg, neck, waist, shoulder, arm,  chest, bicep, wrist, back, stomach, hip, thigh } = req.body        
         const cloth = new Product({ ownerName, contact, price, deposit, deliveryDate, measurements, clothImages, leg, neck, waist, shoulder, arm,  chest, bicep, wrist, back, stomach, hip, thigh });
         cloth.clothImages = req.files.map(f => ({ url: f.path, filename: f.filename }))
         cloth.owner = req.user._id;
         await cloth.save();
-        console.log(cloth);
         req.flash('success', 'successfully made a new cloth');
         res.redirect('/clothings');
-        // res.send(`${cloth.ownerName} has deposited ${ cloth.deposit} and it must be delivered on ${ cloth.deliveryDate }`);
-       
     } catch (error) {
         console.log('clothing error' + error)
-        // req.flash('error', `${error}`);  
         res.send(`Error:${error}`)
     }
 }
@@ -82,8 +62,6 @@ module.exports.renderEditCloth = async (req, res) => {
         }
         res.render('cloth/edit', { cloth })
     } catch(error) {
-        
-        // req.flash('error', `${error}`)
         console.log(error);  
         res.send(`Error:${error}`)
         req.flash('error', `cannot get error: ${error}`)
@@ -97,20 +75,20 @@ module.exports.updateCloth = async (req, res) => {
         const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
         cloth.clothImages.push(...imgs);
         cloth.save();
-        // await cloth.save();
         if(req.body.deleteClothImages) {
             for(let filename of req.body.deleteClothImages) {
                 await cloudinary.uploader.destroy(filename);
             }
             await cloth.updateOne({ $pull: { clothImages: { filename: { $in: req.body.deleteClothImages } } } })
         }
-        console.log(`Weldone ${cloth.ownerName}'s measurements  has been Updated`)
+        // console.log(`Weldone ${cloth.ownerName}'s measurements  has been Updated`)
         req.flash('success', `Weldone ${cloth.ownerName}'s measurements has been Updated`)
         res.redirect(`/clothing/${cloth._id}`)
-    } catch(error) {
-        // console.log(error);        
-        res.send(`Error:${error}`)
-        // req.flash('error', `error due to ${error}`);
+    } catch(error) {     
+        console.log(error);
+        req.flash('error', ` ${error}`);
+        // res.send(`Error:${error}`)
+        res.redirect(`/clothings`);
     }
 }
 
@@ -118,7 +96,6 @@ module.exports.deleteCloth = async (req, res) => {
     try{
         const { id } = req.params;
         const cloth = await Product.findByIdAndDelete(id);
-        console.log(`Hurray ${cloth.ownerName}'s Measurement has been deleted`)
         req.flash('success', `Hurray ${cloth.ownerName}'s Measurement has been deleted`)
         res.redirect('/clothings');
 
@@ -128,18 +105,15 @@ module.exports.deleteCloth = async (req, res) => {
     }
 }
 
-
 module.exports.getClothById = async (req, res) => {
     try {
         const { id } = req.params;
-        const cloth = await Product.findById(id, { ...req.body }).populate('owner');
-        // const cloth = await Product.findById(id, { ...req.body });
-        console.log(cloth);
+        const cloth = await Product.findById(id).populate('owner');
         if(!cloth) {
             req.flash('error', 'This cloth is not available')
             return res.redirect('/clothings')
         }
-        console.log(cloth);
+        // console.log(cloth);
         res.render('cloth/show', { cloth })
     } catch(error) {
         console.log(error);
