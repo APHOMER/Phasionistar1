1a959042896a6008779a746aa49f077f2fe569f8
Aphomer <aaphomer@gmail.com>
2023-07-25 13:13:43 +0100

    re-adding cloudinary

--- cloudinary/cloud.js
+++ cloudinary/cloud.js
@@ -1,27 +1,10 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-// cloudinary.config({ 
-//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-//     api_key: process.env.CLOUDINARY_KEY, 
-//     api_secret: process.env.CLOUDINARY_SECRET
-//   });
-
-https = require('https');
-
-// options
-var options = {
-    host: 'eternagame.wikia.com',
-    path: '/wiki/EteRNA_Dictionary'
-}
-
-// // get
-// https.get(options, callback);
-
 cloudinary.config({ 
-    cloud_name: 'dfhhjkekb', 
-    api_key: '986687577347183', 
-    api_secret: 'HlTJiIl_7Yoi1UiJSgrHKLAPXts' 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_KEY, 
+    api_secret: process.env.CLOUDINARY_SECRET
   });
 
 
